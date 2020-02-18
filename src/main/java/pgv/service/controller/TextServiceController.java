eb0b56f73bc418557a2ea515d9b3ce003d21a71f
Sergio Garcia <sergiogarciadelgado25@gmail.com>
2020-02-18 10:40:50 +0000

    final final commit

--- src/main/java/pgv/service/controller/TextServiceController.java
+++ src/main/java/pgv/service/controller/TextServiceController.java
@@ -25,45 +25,46 @@ import javafx.stage.FileChooser.ExtensionFilter;
 import pgv.service.app.TextServiceApp;
 
 public class TextServiceController implements Initializable {
-	
+
 	private String IP = "10.2.3.20";
 //	private String IP = "10.2.3.12";
-	
-	
-    @FXML
-    private BorderPane root;
 
-    @FXML
-    private Button importarButton;
+	@FXML
+	private BorderPane root;
 
-    @FXML
-    private Button eliminarButton;
+	@FXML
+	private Button importarButton;
 
-    @FXML
-    private Button exportarButton;
+	@FXML
+	private Button eliminarButton;
 
-    @FXML
-    private ListView<String> lista;
-    
-    @FXML
-    private Button refreshButton;
+	@FXML
+	private Button exportarButton;
 
-	   
-	
+	@FXML
+	private ListView<String> lista;
+
+	@FXML
+	private Button refreshButton;
+
 	public Button getImportarButton() {
 		return importarButton;
 	}
+
 	public Button getEliminarButton() {
 		return eliminarButton;
 	}
+
 	public Button getExportarButton() {
 		return exportarButton;
 	}
+
 	public ListView<String> getLista() {
 		return lista;
 	}
+
 	public TextServiceController() {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/View.fxml"));
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/View.fxml"));
 		loader.setController(this);
 		try {
 			loader.load();
@@ -71,141 +72,130 @@ public class TextServiceController implements Initializable {
 			e.printStackTrace();
 		}
 	}
+
 	public void initialize(URL location, ResourceBundle resources) {
-	
+		exportarButton.disableProperty().bind(lista.getSelectionModel().selectedIndexProperty().isEqualTo(-1));
+		eliminarButton.disableProperty().bind(lista.getSelectionModel().selectedIndexProperty().isEqualTo(-1));
 	}
+
 	public BorderPane getRoot() {
 		return root;
 	}
+
 	public void listar() {
 		try {
-			System.out.println("Creando socket cliente");
 			Socket clientSocket = new Socket();
-			System.out.println ("Estableciendo la conexión");
 			InetSocketAddress addr = new InetSocketAddress(IP, 5555);
 			clientSocket.connect(addr);
-			
+
 			DataInputStream entrada = new DataInputStream(clientSocket.getInputStream());
 			DataOutputStream salida = new DataOutputStream(clientSocket.getOutputStream());
 			try {
-					salida.writeUTF("lista");
-					getLista().getItems().setAll(entrada.readUTF().split(","));
+				salida.writeUTF("lista");
+				getLista().getItems().setAll(entrada.readUTF().split(","));
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 
-			System.out.println("Mensaje enviado");
 
-			System.out.println("Cerrando el socket cliente");
 			salida.close();
 			entrada.close();
 			clientSocket.close();
-			System.out.println("Terminado");
-		} catch (IOException e) { e.printStackTrace(); }
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-	
+
 	@FXML
-    void onEliminarAction(ActionEvent event) throws IOException {
-		System.out.println("Creando socket cliente");
+	void onEliminarAction(ActionEvent event) throws IOException {
 		Socket clientSocket = new Socket();
-		System.out.println ("Estableciendo la conexión");
 		InetSocketAddress addr = new InetSocketAddress(IP, 5555);
 		clientSocket.connect(addr);
-		
+
 		DataOutputStream salida = new DataOutputStream(clientSocket.getOutputStream());
 		try {
-				salida.writeUTF("eliminar:"+getLista().getSelectionModel().getSelectedItem());
+			salida.writeUTF("eliminar:" + getLista().getSelectionModel().getSelectedItem());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
-		System.out.println("Mensaje enviado");
 
-		System.out.println("Cerrando el socket cliente");
 		salida.close();
 		clientSocket.close();
-		System.out.println("Terminado");
-		
-		listar();
-    }
 
-    @FXML
-    void onExportarButton(ActionEvent event) throws IOException {
-    	System.out.println("Creando socket cliente");
+		listar();
+	}
+
+	@FXML
+	void onExportarButton(ActionEvent event) throws IOException {
 		Socket clientSocket = new Socket();
-		System.out.println ("Estableciendo la conexión");
 		InetSocketAddress addr = new InetSocketAddress(IP, 5555);
 		clientSocket.connect(addr);
-		
+
 		DataInputStream entrada = new DataInputStream(clientSocket.getInputStream());
 		DataOutputStream salida = new DataOutputStream(clientSocket.getOutputStream());
-		
-		BufferedWriter bw  = new BufferedWriter(new FileWriter("Txt/"+getLista().getSelectionModel().getSelectedItem()));
-		salida.writeUTF("exportar:"+getLista().getSelectionModel().getSelectedItem());
+
+		BufferedWriter bw = new BufferedWriter(
+				new FileWriter("Txt/" + getLista().getSelectionModel().getSelectedItem()));
+		salida.writeUTF("exportar:" + getLista().getSelectionModel().getSelectedItem());
 		try {
-			while(true) {
-				bw.write(entrada.readUTF()+"\n");
+			while (true) {
+				bw.write(entrada.readUTF() + "\n");
 			}
 		} catch (Exception e) {
 			System.out.println("Exportado");
 			bw.close();
 		}
 
-		System.out.println("Mensaje enviado");
 
-		System.out.println("Cerrando el socket cliente");
 		entrada.close();
 		salida.close();
 		clientSocket.close();
-		System.out.println("Terminado");
-		
-		listar();
-    }
 
-    @FXML
-    void onImportarAction(ActionEvent event) throws IOException {
-    	System.out.println("Creando socket cliente");
+		listar();
+	}
+
+	@FXML
+	void onImportarAction(ActionEvent event) throws IOException {
 		Socket clientSocket = new Socket();
-		System.out.println ("Estableciendo la conexión");
 		InetSocketAddress addr = new InetSocketAddress(IP, 5555);
 		clientSocket.connect(addr);
-		
+
 		DataInputStream entrada = new DataInputStream(clientSocket.getInputStream());
 		DataOutputStream salida = new DataOutputStream(clientSocket.getOutputStream());
-		
+
 		FileChooser fc = new FileChooser();
-		fc.getExtensionFilters().addAll(new ExtensionFilter("txt files","*.txt"));
-		
+		fc.getExtensionFilters().addAll(new ExtensionFilter("txt files", "*.txt"));
+
 		fc.setTitle("Importar Txt");
-		
+
 		File fichero = fc.showOpenDialog(TextServiceApp.getStage());
-		BufferedReader br  = new BufferedReader(new FileReader(fichero));
-		salida.writeUTF("importar:"+fichero.getName());
-		String linea;
-		try {
-			while((linea = br.readLine())!=null) {
-				salida.writeUTF(linea);
+		if (fichero != null) {
+			BufferedReader br = new BufferedReader(new FileReader(fichero));
+			salida.writeUTF("importar:" + fichero.getName());
+			String linea;
+			try {
+				while ((linea = br.readLine()) != null) {
+					salida.writeUTF(linea);
+				}
+			} catch (Exception e) {
+				System.out.println("Importado");
+				br.close();
 			}
-		} catch (Exception e) {
-			System.out.println("Importado");
-			br.close();
+		}else {
+			salida.writeUTF("abortado");
 		}
 
-		System.out.println("Mensaje enviado");
-
-		System.out.println("Cerrando el socket cliente");
 		entrada.close();
 		salida.close();
 		clientSocket.close();
-		System.out.println("Terminado");
-		
-		listar();
-    }
 
-    @FXML
-    void onRefreshAction(ActionEvent event) {
-    	listar();
-    }
-	
-	
+		listar();
+	}
+
+	@FXML
+	void onRefreshAction(ActionEvent event) {
+		listar();
+	}
+
 }
--- src/main/java/pgv/service/servidor/TextServidor.java
+++ src/main/java/pgv/service/servidor/TextServidor.java
@@ -3,6 +3,7 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.EOFException;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -25,7 +26,6 @@ public class TextServidor {
 		}
 		Socket cs;
 		File directorio = new  File("Txt");
-//		ArrayList<String> clientes = new ArrayList<String>(); 
 		try {
 			System.out.println(" - Servidor creado -\n\n");
 			while (true) {
@@ -39,46 +39,47 @@ public class TextServidor {
 				
 				System.out.println(" - Cliente Conectado - \n\n");
 				String recibido = dis.readUTF();
-				if(recibido.equals("lista")) {
-					System.out.println(" - Lista - \n\n");
-					String[] cosa = directorio.list();
-					for(int i= 0;i<cosa.length;i++) {
-						aux.append(cosa[i]+",");
-					}
-					
-					respuesta = aux.toString();
-					dos.writeUTF(respuesta);
-				}else {
-					String[] arrayRecibido = recibido.split(":");
-					String orden = arrayRecibido[0];
-					String archivo = arrayRecibido[1];
-					if(orden.equals("eliminar")) {
-						System.out.println(new File("Txt/"+archivo).delete());
-						
-					}else if(orden.equals("importar")) {
-						BufferedWriter bw = new BufferedWriter( new FileWriter("Txt/"+archivo));
-						try {
-							while(true) {
-								bw.write(dis.readUTF()+"\n");
+				if (!recibido.equals("abortado")) {
+					if (recibido.equals("lista")) {
+						System.out.println(" - Lista - \n\n");
+						String[] cosa = directorio.list();
+						for (int i = 0; i < cosa.length; i++) {
+							aux.append(cosa[i] + ",");
+						}
+
+						respuesta = aux.toString();
+						dos.writeUTF(respuesta);
+					} else {
+						String[] arrayRecibido = recibido.split(":");
+						String orden = arrayRecibido[0];
+						String archivo = arrayRecibido[1];
+						if (orden.equals("eliminar")) {
+							new File("Txt/" + archivo).delete();
+						} else if (orden.equals("importar")) {
+							BufferedWriter bw = new BufferedWriter(new FileWriter("Txt/" + archivo));
+							try {
+								while (true) {
+									bw.write(dis.readUTF() + "\n");
+								}
+							} catch (Exception e) {
+								bw.close();
 							}
-						} catch (Exception e) {
-							bw.close();
+						} else if (orden.equals("exportar")) {
+							BufferedReader br = new BufferedReader(new FileReader("Txt/" + archivo));
+							String aux2;
+							while ((aux2 = br.readLine()) != null) {
+								dos.writeUTF(aux2);
+							}
+							br.close();
+							dos.close();
 						}
-					}else if(orden.equals("exportar")) {
-						BufferedReader br = new BufferedReader( new FileReader("Txt/"+archivo));
-						String aux2;
-						while((aux2 = br.readLine())!= null) {
-							dos.writeUTF(aux2);
-						}
-						br.close();
-						dos.close();
-					}
-				}
-				
-				
+						System.out.println(" - Orden realizada : "+orden+" - \n\n");
+					} 
+				}	
+				System.out.println(" - Cliente desconectado - \n\n");
 			}
 
-		} catch (Exception e) { e.printStackTrace(); }finally {
+		} catch (Exception  e) { e.printStackTrace(); }finally {
 		
 		}
 	}
